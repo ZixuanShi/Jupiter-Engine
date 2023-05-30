c175f9f8da632fdf60929500f638d4964366bfb9
Yuki-SHI0726 <szx0726@gmail.com>
2023-05-30 12:25:07 -0700

    Refactored Helpers RETURN_IF_XXX series to make it shorter and cleaner

--- Source/Core/Helpers/Helpers.h
+++ Source/Core/Helpers/Helpers.h
@@ -1,21 +1,9 @@
 #pragma once
 
-// If the condition has occured, log an error message and return false
+// If the condition has occured, log an message and return a value
 // - condition: The expression of the operation
-// - failedMessage: The message to log if expectedExpression failed
-#define RETURN_FALSE_IF_LOG(condition, message, ...)\
-	if((condition))\
-	{\
-		JPT_ERROR(message, __VA_ARGS__);\
-		return false;\
-	}
-
-#define RETURN_FALSE_IF(condition)\
-	if((condition))\
-	{\
-		return false;\
-	}
-
+// - returnValue: The value to return if condition occured
+// - message: The message to log if condition occured
 #define RETURN_VALUE_IF_LOG(condition, returnValue, message, ...)\
 	if ((condition))\
 	{\
@@ -29,18 +17,17 @@
 		return returnValue;\
 	}
 
-#define RETURN_IF_LOG(condition, returnValue, message, ...)\
-	if ((condition))\
-	{\
-		JPT_LOG(message, __VA_ARGS__);\
-		return;\
-	}
+#define RETURN_FALSE_IF_LOG(condition, message, ...)\
+	RETURN_VALUE_IF_LOG(condition, false, message, __VA_ARGS__);
 
-#define RETURN_IF(condition, returnValue)\
-	if ((condition))\
-	{\
-		return;\
-	}
+#define RETURN_FALSE_IF(condition)\
+	RETURN_VALUE_IF(condition, false);
+
+#define RETURN_IF_LOG(condition, message, ...)\
+	RETURN_VALUE_IF_LOG(condition, void(), message, __VA_ARGS__);
+
+#define RETURN_IF(condition)\
+	RETURN_VALUE_IF(condition, void());
 
 // Helper for clean and delete.
 // - pPointer: Assumed to have Clean() function implemented
