824b0b350dcf1304ac53e484b095204f0dd3b467
Yuki-SHI0726 <szx0726@gmail.com>
2023-06-10 21:44:40 -0700

    refactor: CrtDumpMemoryLeaks was reporting false memory leaks for static heap-allocating objects. Switched to CrtSetDbgFlag method to avoid

--- Source/ApplicationLayer/Applications/EntryPoint.h
+++ Source/ApplicationLayer/Applications/EntryPoint.h
@@ -1,6 +1,13 @@
 // Copyright Jupiter Technologies, Inc. All Rights Reserved.
 
 #pragma once
+
+#if IS_DEBUG
+	#define _CRTDBG_MAP_ALLOC
+	#include <stdlib.h>
+	#include <crtdbg.h>
+#endif
+
 #include "ApplicationLayer/Applications/ApplicationBase.h"
 
 /* Designed to be implemented in client game C++
@@ -8,6 +15,7 @@
 extern jpt::ApplicationBase* CreateApplication();
 
 #if IS_PLATFORM_WIN64
+
 #include "ApplicationLayer/Applications/Platform_Win64/Win64Application.h"
 
 #include <windows.h>
@@ -15,6 +23,11 @@ extern jpt::ApplicationBase* CreateApplication();
 _Use_decl_annotations_
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
+#if IS_DEBUG
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
+#endif
+
 	jpt::Win64Application* pApplication = static_cast<jpt::Win64Application*>(CreateApplication());
 	pApplication->SetHINSTANCE(hInstance);
 	pApplication->SetnCmdShow(nCmdShow);
@@ -28,14 +41,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 	pApplication->Clean();
 	JPT_SAFE_DELETE(pApplication);
 
-	_CrtDumpMemoryLeaks();
-
 	return 0;
 }
 
 #else
 int main(int, char**)
 {
+#if IS_DEBUG
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
+#endif
+
 	jpt::ApplicationBase* pApplication = CreateApplication();
 
 	if (pApplication->Init())
@@ -45,8 +61,6 @@ int main(int, char**)
 
 	pApplication->Clean();
 	JPT_DELETE(pApplication);
-
-	_CrtDumpMemoryLeaks();
 
 	return 0;
 }
