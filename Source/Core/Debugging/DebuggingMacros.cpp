712e01f94cf8d4fcb05da4a061f0488ad0151afb
Yuki-SHI0726 <szx0726@gmail.com>
2023-07-02 14:13:04 -0700

    feat: Pushed JPT_ASSERT to another level! Now it takes as many variadic arguments as I want

--- Source/Core/Debugging/DebuggingMacros.cpp
+++ Source/Core/Debugging/DebuggingMacros.cpp
@@ -1,3 +1,37 @@
 // Copyright Jupiter Technologies, Inc. All Rights Reserved.
 
 #include "JupiterPCH.h"
+
+namespace jpt
+{
+#if JPT_ENABLE_ASSERTS
+	void OnAssertionFailed(int32 line, const char* file, const char* expression, const char* format, ...)
+	{
+		jpt::string message = "Assertion Failed: ";
+		message.append(expression);
+
+		if (format)
+		{
+			message.append(", ");
+
+			char messageBuffer[512];
+
+			va_list args;
+			va_start(args, format);
+
+			vsprintf_s(messageBuffer, format, args);
+
+			va_end(args);
+
+			message.append(messageBuffer);
+		}
+
+		jpt::Logger::GetInstance().Log(jpt::Logger::ELogType::Error, line, file, message.c_str());
+	}
+
+	void OnAssertionFailed(int32 line, const char* file, const char* expression)
+	{
+		OnAssertionFailed(line, file, expression, nullptr);
+	}
+#endif
+}
--- Source/Core/Debugging/DebuggingMacros.h
+++ Source/Core/Debugging/DebuggingMacros.h
@@ -2,24 +2,41 @@
 
 #pragma once
 
+namespace jpt
+{
 #if JPT_ENABLE_ASSERTS
+
+	/** Implementation when an assertion has failed
+		@param line:	   Line of the file caused assertion fail
+		@param file:       The file where assertion failed 
+		@param expression: Stringified assert condition.
+		@param format:     Optional. Used to append additional debug info if there's any provided. Follow printf format
+		@param ... :       Optional. Variadic arguments to append to assertion. */
+	void OnAssertionFailed(int32 line, const char* file, const char* expression, const char* format, ...);
+	void OnAssertionFailed(int32 line, const char* file, const char* expression);
 
 	/** Assert with error message logging if Debug mode, do nothing if not Debugging
 		@param expression: A boolean expression to assert true
-		@param ...:	       Error messages if the expression went false */
+		@param ...:	       Error messages if the expression went false 
+
+		Example:
+		int a = 10;
+		int b = 11;
+		JPT_ASSERT(a == 9 && b == 10, "expected 9 and 10, received %d and %d", a, b);*/
 	#define JPT_ASSERT(expression, ...) \
 		if (!(expression))\
 		{\
 			if (JPT_HAS_ARGS(#__VA_ARGS__))\
 			{\
-				JPT_ERROR("Assertion Failed: "#expression##", %s", __VA_ARGS__);\
+				jpt::OnAssertionFailed(__LINE__, __FILE__, #expression, __VA_ARGS__);\
 			}\
 			else\
 			{\
-				JPT_ERROR("Assertion Failed: "#expression);\
+				jpt::OnAssertionFailed(__LINE__, __FILE__, #expression);\
 			}\
 			__debugbreak();\
 		}
 #else
 	#define JPT_ASSERT(expression, ...) 
 #endif
+}
