683bfa54b040f9fb2f130c19f50760a7f25de66e
Zixuan Shi <szx0726@gmail.com>
2024-03-06 18:13:19 -0800

    refactor: setting args variable in batch files, making it cleaner for later custom args flexibility

--- Projects/Benchmarks/Scripts/GenerateProjects.bat
+++ Projects/Benchmarks/Scripts/GenerateProjects.bat
@@ -1,3 +1,6 @@
 cd "C:\Program Files\Jupiter Technologies\Jupiter-Engine\Scripts"
-call "C:\Program Files\Jupiter Technologies\Jupiter-Engine\Tools\Premake\Generated\premake5.exe" vs2022 "Benchmarks" "C:/Program Files/Jupiter Technologies/Jupiter-Engine/Projects/Benchmarks/"
+
+set args=vs2022 "Benchmarks" "C:/Program Files/Jupiter Technologies/Jupiter-Engine/Projects/Benchmarks/"
+call "C:\Program Files\Jupiter Technologies\Jupiter-Engine\Tools\Premake\Generated\premake5.exe" %args%
+
 pause
