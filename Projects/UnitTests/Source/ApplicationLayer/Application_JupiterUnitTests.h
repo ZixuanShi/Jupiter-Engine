bbce37e595573ec706bd4858cee061ab2046ec38
Zixuan Shi <szx0726@gmail.com>
2024-07-16 23:01:00 -0700

    removed testing code

--- Projects/UnitTests/Source/ApplicationLayer/Application_JupiterUnitTests.h
+++ Projects/UnitTests/Source/ApplicationLayer/Application_JupiterUnitTests.h
@@ -6,9 +6,6 @@ import jpt.Application_Base;
 
 class Application_JupiterUnitTests final : public jpt::Application_Base
 {
-private:
-	using Super = jpt::Application_Base;
-
 public:
 	virtual bool Init() override final;
 };
