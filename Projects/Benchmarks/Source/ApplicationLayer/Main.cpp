f86a787d16b349de9480c3ee5d642610107a85fa
Zixuan Shi <szx0726@gmail.com>
2024-07-18 12:05:55 -0700

    fix: Updated Benchmarks project to adapt latest change in ApplicationLayer

--- Projects/Benchmarks/Source/ApplicationLayer/Main.cpp
+++ Projects/Benchmarks/Source/ApplicationLayer/Main.cpp
@@ -1,9 +1,5 @@
 // Copyright Jupiter Technologies, Inc. All Rights Reserved.
 
-#include "Core/Minimal/CoreHeaders.h"
-#include "Application_Benchmarks.h"
-
-import jpt.CoreModules;
 import jpt.EntryPoints;
 
 /** Main entry point for different platforms */
@@ -14,9 +10,6 @@ import jpt.EntryPoints;
 _Use_decl_annotations_
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lPStr, int nCmdShow)
 {
-	JPT_SCOPED_TIMING_PROFILER("Main");
-	JPT_LOG("Benchmarking Results:");
-
-	return jpt::MainImplWin64(hInstance, hPrevInstance, lPStr, nCmdShow);
+	return jpt::MainImpl_Win64(hInstance, hPrevInstance, lPStr, nCmdShow);
 }
 #endif
--- Projects/Benchmarks/Source/Core/Containers/Benchmarks_DynamicArray.ixx
+++ Projects/Benchmarks/Source/Core/Containers/Benchmarks_DynamicArray.ixx
@@ -1,5 +1,9 @@
 // Copyright Jupiter Technologies, Inc. All Rights Reserved.
 
+module;
+
+#include <vector>
+
 export module Benchmarks_DynamicArray;
 
 import jpt.DynamicArray;
