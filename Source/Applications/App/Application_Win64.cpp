df45b981e7d7142814a20d69044ca64d6eb72b09
Zixuan Shi <szx0726@gmail.com>
2024-07-24 03:25:05 -0700

    feat: Add "exe_path" to CommandLine for win64

--- Source/Applications/App/Application_Win64.cpp
+++ Source/Applications/App/Application_Win64.cpp
@@ -5,12 +5,22 @@
 #include "Core/Minimal/CoreMacros.h"
 #include "Debugging/Logger.h"
 
+import jpt.CommandLine;
+import jpt.StringUtils;
+
 namespace jpt
 {
 	bool Application_Win64::PreInit()
 	{
 		JPT_ENSURE(Super::PreInit());
 		JPT_ENSURE(m_hInstance != nullptr);
+
+		wchar_t exePath[MAX_PATH];
+		GetModuleFileName(m_hInstance, exePath, MAX_PATH);
+
+		String exePathStr;
+		exePathStr.MoveString(ToChars(exePath));
+		CommandLine::GetInstance().Add("exe_path", exePathStr);
 
 		return true;
 	}
