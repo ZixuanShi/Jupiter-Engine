6b3e59cd4142d68a34a6121dce08cd8d82e95b6f
Zixuan Shi <szx0726@gmail.com>
2024-10-20 00:35:27 -0700

    refactor: All platforms are set within the same solution file

--- Projects/Blank/Scripts/GenerateProjectFiles_vs2022_Android.bat
+++
@@ -1,3 +0,0 @@
-set args="platform=android"
-call "C:\Program Files\Jupiter Technologies\Jupiter-Engine\Tools\Premake\Bin\premake5.exe" vs2022 %args%
-pause
---
+++ Projects/UnitTests/Scripts/GenerateProjectFiles_vs2022.bat
@@ -0,0 +1,2 @@
+call "C:\Program Files\Jupiter Technologies\Jupiter-Engine\Tools\Premake\Bin\premake5.exe" vs2022
+pause
