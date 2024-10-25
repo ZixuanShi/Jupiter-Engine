47690921e30ef1b40cde31ef05301e85c9abb8af
Zixuan Shi <szx0726@gmail.com>
2024-10-24 22:08:28 -0700

    feat: Clean Generated Folders script

---
+++ Scripts/CleanGeneratedFolders.py
@@ -0,0 +1,29 @@
+# Deletes all generated folders in Jupiter Engine
+
+import os
+import shutil
+
+to_remove = ["_Bin", "_ProjectFiles", "_Saved"]
+
+def Clean(root):
+    print("Cleaning " + root)
+    for folder in to_remove:
+        if os.path.exists(root + folder):
+            print("Removing " + root + folder)
+            shutil.rmtree(root + folder)
+
+if __name__ == "__main__":
+    # Jupiter Root
+    jupiter_root = os.getenv("JUPITER_ENGINE_DIR")
+    if jupiter_root is None:
+        jupiter_root = "C:/Program Files/Jupiter Technologies/Jupiter-Engine/"
+
+    Clean(jupiter_root)
+
+    # Projects
+    projects = os.listdir(jupiter_root + "Projects")
+    for project in projects:
+        project_path = jupiter_root + "Projects/" + project + "/"
+        Clean(project_path)
+
+    print("Finished cleaning Jupiter Engine")
