5b7066fdf5b88d07fb4373434907b2884daa7a7e
Zixuan Shi <szx0726@gmail.com>
2024-11-13 23:28:03 -0800

    refactor: hlsl don't need to be copied to packages, only the spv do

--- Scripts/CompileShaders.bat
+++ Scripts/CompileShaders.bat
@@ -1,8 +1,8 @@
 @echo off
 if not exist "%JUPITER_ENGINE_DIR%_Baked/Shaders" mkdir "%JUPITER_ENGINE_DIR%_Baked/Shaders"
 
-"%VULKAN_SDK%/Bin/dxc.exe" -spirv -T vs_6_0 -E main "%JUPITER_ENGINE_DIR%Assets/Jupiter_Common/Shaders/triangle.vs.hlsl" -Fo "%JUPITER_ENGINE_DIR%_Baked/Shaders/triangle.vs.spv"
-"%VULKAN_SDK%/Bin/dxc.exe" -spirv -T ps_6_0 -E main "%JUPITER_ENGINE_DIR%Assets/Jupiter_Common/Shaders/triangle.ps.hlsl" -Fo "%JUPITER_ENGINE_DIR%_Baked/Shaders/triangle.ps.spv"
+"%VULKAN_SDK%/Bin/dxc.exe" -spirv -T vs_6_0 -E main "%JUPITER_ENGINE_DIR%Assets/Shaders/triangle.vs.hlsl" -Fo "%JUPITER_ENGINE_DIR%_Baked/Shaders/triangle.vs.spv"
+"%VULKAN_SDK%/Bin/dxc.exe" -spirv -T ps_6_0 -E main "%JUPITER_ENGINE_DIR%Assets/Shaders/triangle.ps.hlsl" -Fo "%JUPITER_ENGINE_DIR%_Baked/Shaders/triangle.ps.spv"
 
 if %ERRORLEVEL% EQU 0 (
     echo Shader compilation successful!
