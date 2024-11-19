35a20bcd14c121f0ba93357601cdc3d614075a09
Zixuan Shi <szx0726@gmail.com>
2024-11-18 22:30:58 -0800

    refactor: Var for shaders output and source dir

--- Scripts/CompileShaders.bat
+++ Scripts/CompileShaders.bat
@@ -1,8 +1,13 @@
 @echo off
+
+set "SHADER_SOURCE_DIR=%JUPITER_ENGINE_DIR%Assets/Shaders"
+set "SHADER_BAKED_DIR=%JUPITER_ENGINE_DIR%_Baked/Shaders"
+
+REM Common shaders, should be packed to client project release package
 if not exist "%JUPITER_ENGINE_DIR%_Baked/Shaders" mkdir "%JUPITER_ENGINE_DIR%_Baked/Shaders"
 
-"%VULKAN_SDK%/Bin/dxc.exe" -spirv -T vs_6_0 -E main "%JUPITER_ENGINE_DIR%Assets/Shaders/triangle.vs.hlsl" -Fo "%JUPITER_ENGINE_DIR%_Baked/Shaders/triangle.vs.spv"
-"%VULKAN_SDK%/Bin/dxc.exe" -spirv -T ps_6_0 -E main "%JUPITER_ENGINE_DIR%Assets/Shaders/triangle.ps.hlsl" -Fo "%JUPITER_ENGINE_DIR%_Baked/Shaders/triangle.ps.spv"
+"%VULKAN_SDK%/Bin/dxc.exe" -spirv -T vs_6_0 -E main "%SHADER_SOURCE_DIR%/triangle.vs.hlsl" -Fo "%SHADER_BAKED_DIR%/triangle.vs.spv"
+"%VULKAN_SDK%/Bin/dxc.exe" -spirv -T ps_6_0 -E main "%SHADER_SOURCE_DIR%/triangle.ps.hlsl" -Fo "%SHADER_BAKED_DIR%/triangle.ps.spv"
 
 if %ERRORLEVEL% EQU 0 (
     echo Shader compilation successful!
