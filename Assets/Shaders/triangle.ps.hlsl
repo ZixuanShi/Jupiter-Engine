10d0ad8a672899c4ca0d710d0fe047077eb1dde0
ZixuanShi <szx0726@gmail.com>
2024-12-17 15:38:25 -0800

    Updated hlsl shaders to match current functionality

--- Assets/Shaders/triangle.ps.hlsl
+++ Assets/Shaders/triangle.ps.hlsl
@@ -1,10 +1,9 @@
-struct PSInput 
+struct PSInput
 {
-    float4 position : SV_POSITION;
     float3 color : COLOR0;
 };
 
-float4 main(PSInput input) : SV_TARGET0 
+float4 main(PSInput input) : SV_TARGET
 {
     return float4(input.color, 1.0);
 }
--- Assets/Shaders/triangle.vs.hlsl
+++ Assets/Shaders/triangle.vs.hlsl
@@ -1,10 +1,3 @@
-cbuffer UniformBufferObject : register(b0)
-{
-    matrix model;
-    matrix view;
-    matrix proj;
-};
-
 struct VertexInput
 {
     float2 position : POSITION;
@@ -21,10 +14,7 @@ VertexOutput main(VertexInput input)
 {
     VertexOutput output;
     
-    float4 positionWorld = mul(float4(input.position, 0.0f, 1.0f), model);
-    float4 positionView = mul(positionWorld, view);
-    output.position = mul(positionView, proj);
-    
+    output.position = float4(input.position, 0.0, 1.0);
     output.color = input.color;
     
     return output;
