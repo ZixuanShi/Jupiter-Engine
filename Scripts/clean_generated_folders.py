7c9f2c201b31794abc1c4651c4abf0603da22328
ZixuanShi <szx0726@gmail.com>
2025-02-27 21:42:38 -0800

    refactor: Scripts naming convention and coding standards

--- Scripts/clean_generated_folders.py
+++ Scripts/clean_generated_folders.py
@@ -1,13 +1,13 @@
-# Deletes all generated folders in Jupiter Engine
+# Deletes all generated temp folders in Jupiter Engine
 
 import os
 import shutil
 
-to_remove = ["_Output", "_ProjectFiles", "_Saved", "_Baked"]
+to_delete = ["_Output", "_ProjectFiles", "_Saved", "_Baked"]
 
-def Clean(root):
+def clean(root):
     print("Cleaning " + root)
-    for folder in to_remove:
+    for folder in to_delete:
         if os.path.exists(root + folder):
             print("Removing " + root + folder)
             shutil.rmtree(root + folder)
@@ -18,12 +18,12 @@ if __name__ == "__main__":
     if jupiter_root is None:
         jupiter_root = "C:/Program Files/Jupiter Technologies/Jupiter-Engine/"
 
-    Clean(jupiter_root)
+    clean(jupiter_root)
 
     # Projects
     projects = os.listdir(jupiter_root + "Projects")
     for project in projects:
         project_path = jupiter_root + "Projects/" + project + "/"
-        Clean(project_path)
+        clean(project_path)
 
     print("Finished cleaning Jupiter Engine")
--- Scripts/compile_shaders.bat
+++ Scripts/compile_shaders.bat
@@ -5,7 +5,6 @@ set "SHADER_BAKED_DIR=%JUPITER_ENGINE_DIR%_Baked/Jupiter_Common/Shaders"
 
 if not exist "%SHADER_BAKED_DIR%" mkdir "%SHADER_BAKED_DIR%"
 
-
 "%VULKAN_SDK%/Bin/glslc.exe" -fshader-stage=vertex "%SHADER_SOURCE_DIR%/sample_vert.glsl" -o "%SHADER_BAKED_DIR%/sample_vert_glsl.spv"
 "%VULKAN_SDK%/Bin/glslc.exe" -fshader-stage=fragment "%SHADER_SOURCE_DIR%/sample_frag.glsl" -o "%SHADER_BAKED_DIR%/sample_frag_glsl.spv"
 
