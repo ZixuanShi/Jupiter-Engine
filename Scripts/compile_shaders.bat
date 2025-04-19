1521a876b5089155e07df1d31c6cad92c98c4202
ZixuanShi <szx0726@gmail.com>
2025-04-19 00:51:03 -0700

    clean compute shader leftover

--- Scripts/compile_shaders.bat
+++ Scripts/compile_shaders.bat
@@ -7,7 +7,6 @@ if not exist "%SHADER_BAKED_DIR%" mkdir "%SHADER_BAKED_DIR%"
 
 "%VULKAN_SDK%/Bin/glslc.exe" -fshader-stage=vertex   "%SHADER_SOURCE_DIR%/sample_vert.glsl"    -o "%SHADER_BAKED_DIR%/sample_vert_glsl.spv"
 "%VULKAN_SDK%/Bin/glslc.exe" -fshader-stage=fragment "%SHADER_SOURCE_DIR%/sample_frag.glsl"    -o "%SHADER_BAKED_DIR%/sample_frag_glsl.spv"
-"%VULKAN_SDK%/Bin/glslc.exe" -fshader-stage=fragment "%SHADER_SOURCE_DIR%/particles_comp.glsl" -o "%SHADER_BAKED_DIR%/particles_comp_glsl.spv"
 
 "%VULKAN_SDK%/Bin/glslc.exe" -fshader-stage=vertex   "%SHADER_SOURCE_DIR%/sample_vert.hlsl" -o "%SHADER_BAKED_DIR%/sample_vert_hlsl.spv"
 "%VULKAN_SDK%/Bin/glslc.exe" -fshader-stage=fragment "%SHADER_SOURCE_DIR%/sample_frag.hlsl" -o "%SHADER_BAKED_DIR%/sample_frag_hlsl.spv"
